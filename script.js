c4c80a2cd6a338d2d0ce9108ad87dab40835b9fa
vidik-do <guptaajihihi27@gmail.com>
2024-12-04 22:44:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    // Find the surprise element
+    const surprise = document.getElementById('surprise');
+    // Make it visible
+    surprise.style.display = 'block';
+}
